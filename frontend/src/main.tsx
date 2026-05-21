824b9b25b3410e3bbc1ddf86e7023bdc1759de80
venkatasivanaga <vs.naga@ufl.edu>
2026-05-21 16:42:37 -0400

    build: configure Vite and React entry points for local UI development

---
+++ frontend/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { AgentTrace } from './AgentTrace'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AgentTrace />
+  </React.StrictMode>,
+)
